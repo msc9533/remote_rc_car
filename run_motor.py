b1363d88af18c4069c3fb41335a0fc9fb24b28a8
Seung-Chan Mok <msc9533@gmail.com>
2021-05-20 15:21:03 +0900

    Create run_motor.py

---
+++ run_motor.py
@@ -0,0 +1,58 @@
+import RPi.GPIO as GPIO
+import time
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+GPIO.setup(12, GPIO.OUT)
+GPIO.setup(5, GPIO.OUT)
+GPIO.setup(6, GPIO.OUT)
+GPIO.setup(13, GPIO.OUT)
+
+def stop():
+    GPIO.output(12, GPIO.LOW)
+    GPIO.output(5, GPIO.LOW)
+    
+    GPIO.output(6, GPIO.LOW)
+    GPIO.output(13, GPIO.LOW)
+
+def backward():
+    GPIO.output(12, GPIO.LOW)
+    GPIO.output(5, GPIO.HIGH)
+    
+    GPIO.output(6, GPIO.HIGH)
+    GPIO.output(13, GPIO.LOW)
+    
+
+def forward():
+    GPIO.output(12, GPIO.HIGH)
+    GPIO.output(5, GPIO.LOW)
+    
+    GPIO.output(6, GPIO.LOW)
+    GPIO.output(13, GPIO.HIGH)
+    
+def left():
+    GPIO.output(12, GPIO.LOW)
+    GPIO.output(5, GPIO.HIGH)
+    
+    GPIO.output(6, GPIO.LOW)
+    GPIO.output(13, GPIO.HIGH)
+    
+def right():
+    GPIO.output(12, GPIO.HIGH)
+    GPIO.output(5, GPIO.LOW)
+    
+    GPIO.output(6, GPIO.HIGH)
+    GPIO.output(13, GPIO.LOW)
+
+if __name__ == "__main__":
+    stop()
+    time.sleep(3)
+    forward()
+    time.sleep(3)
+    backward()
+    time.sleep(3)
+    right()
+    time.sleep(3)
+    left()
+    time.sleep(3)
+    stop()
+    GPIO.cleanup()
